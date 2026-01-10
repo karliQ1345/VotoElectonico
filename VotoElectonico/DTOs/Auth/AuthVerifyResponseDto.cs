5b9d1363647c8de9b60af20b7199b6ee35fe953f
Enma <c.enma69@gmail.com>
2026-01-10 09:27:58 -0500

    Prueba de push

--- VotoElectonico/DTOs/Auth/AuthVerifyResponseDto.cs
+++ VotoElectonico/DTOs/Auth/AuthVerifyResponseDto.cs
@@ -2,7 +2,7 @@
 {
     public class AuthVerifyResponseDto
     {
-        public int UsuarioId { get; set; }
+        public int UsuarioId { get; set; } 
         public string Rol { get; set; } = default!;
         public string? AccessToken { get; set; } // si luego metes JWT
     }

6518507b0582e2971e3ade717d41d66af95c5a4e
Enma <c.enma69@gmail.com>
2026-01-10 19:58:34 -0500

    Configurar inyección de dependencias para Email y 2FA.

--- VotoElectonico/Program.cs
+++ VotoElectonico/Program.cs
@@ -1,6 +1,9 @@
 
 using Microsoft.EntityFrameworkCore;
 using VotoElectonico.Data;
+using VotoElectonico.Options;
+using VotoElectonico.Services.Auth;
+using VotoElectonico.Services.Email;
 
 namespace VotoElectonico
 {
@@ -17,6 +20,16 @@ namespace VotoElectonico
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.Configure<BrevoOptions>(builder.Configuration.GetSection("Brevo"));
+            builder.Services.Configure<TwoFactorOptions>(builder.Configuration.GetSection("TwoFactor"));
+
+            builder.Services.AddScoped<ITwoFactorService, TwoFactorService>();
+
+            builder.Services.AddHttpClient<IEmailSender, BrevoEmailSender>(client =>
+            {
+                client.Timeout = TimeSpan.FromSeconds(15);
+            });
 
             var app = builder.Build();
 

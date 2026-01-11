592808283a43d6f7a42e4bf2213aa895978df60b
Enma <c.enma69@gmail.com>
2026-01-10 19:47:36 -0500

    Implementar servicios de Email (Brevo) y seguridad 2FA.

---
+++ VotoElectonico/Services/Auth/ITwoFactorService.cs
@@ -0,0 +1,9 @@
+﻿namespace VotoElectonico.Services.Auth
+{
+    public interface ITwoFactorService
+    {
+        string GenerateOtp(int digits = 6);
+        string HashOtp(string otp);
+        bool VerifyOtp(string otp, string storedHash);
+    }
+}

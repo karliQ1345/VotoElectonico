70cbf4b45b22df5c48ad123f3490cec335251691
Enma <c.enma69@gmail.com>
2026-01-10 19:19:20 -0500

    Creación de la carpeta options con la lógica para Brevo y TwoFactorOptions.

---
+++ VotoElectonico/Options/BrevoOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace VotoElectonico.Options
+{
+    public class BrevoOptions
+    {
+        public string ApiKey { get; set; } = default!;
+        public string FromEmail { get; set; } = default!;
+        public string FromName { get; set; } = default!;
+    }
+}
---
+++ VotoElectonico/Options/TwoFactorOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace VotoElectonico.Options
+{
+    public class TwoFactorOptions
+    {
+        public int CodeTtlMinutes { get; set; } = 5;
+        public int MaxAttempts { get; set; } = 5;
+        public string Pepper { get; set; } = default!;
+    }
+}

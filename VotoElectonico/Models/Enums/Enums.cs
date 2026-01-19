5a4c7017519608b24cf84656f7f30186aef90bc9
karliQ1345 <karlaquelal83@gmail.com>
2026-01-19 08:33:07 -0500

    Carpeta Enums y clase enums

---
+++ VotoElectonico/Models/Enums/Enums.cs
@@ -0,0 +1,49 @@
+﻿namespace VotoElectonico.Models.Enums
+{
+    public enum RolTipo
+    {
+        Administrador = 1,
+        Votante = 2,
+        JefeJunta = 3
+    }
+
+    public enum TwoFactorCanal
+    {
+        Email = 1
+    }
+
+    public enum ProcesoEstado
+    {
+        Pendiente = 1,
+        Activo = 2,
+        Finalizado = 3
+    }
+
+    public enum EleccionTipo
+    {
+        Presidente_SiNoBlanco = 1,
+        Asambleistas = 2
+    }
+
+    public enum ComprobanteEstado
+    {
+        Pendiente = 1,
+        Enviado = 2,
+        Fallido = 3
+    }
+
+    public enum EnvioResultadoEstado
+    {
+        Recibido = 1,
+        Validado = 2,
+        Rechazado = 3
+    }
+
+    public enum DimensionReporte
+    {
+        Provincia = 1,
+        Canton = 2,
+        Parroquia = 3,
+        Genero = 4
+    }
+}

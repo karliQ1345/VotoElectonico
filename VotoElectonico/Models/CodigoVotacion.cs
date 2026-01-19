78976985a202242c9164e1c40458f35fc198d9a3
karliQ1345 <karlaquelal83@gmail.com>
2026-01-19 08:41:06 -0500

    ódigo único de votación (la “llave”)
    Debe ser por proceso y por usuario, y guardado como hash. Y  Proceso electoral, elecciones y candidatos

---
+++ VotoElectonico/Models/CodigoVotacion.cs
@@ -0,0 +1,20 @@
+﻿namespace VotoElectonico.Models
+{
+    public class CodigoVotacion
+    {
+        public Guid Id { get; set; }
+
+        public Guid ProcesoElectoralId { get; set; }
+        public ProcesoElectoral ProcesoElectoral { get; set; } = null!;
+
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; } = null!;
+
+        public string CodigoHash { get; set; } = null!;
+        public bool Usado { get; set; } = false;
+        public DateTime? UsadoUtc { get; set; }
+
+        public DateTime CreadoUtc { get; set; } = DateTime.UtcNow;
+        public DateTime? MostradoAlJefeUtc { get; set; }    // cuando el jefe lo “entrega”
+    }
+}
---
+++ VotoElectonico/Models/ProcesoElectoral.cs
@@ -0,0 +1,17 @@
+﻿using VotoElectonico.Models.Enums;
+
+namespace VotoElectonico.Models
+{
+    public class ProcesoElectoral
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public DateTime InicioUtc { get; set; }
+        public DateTime FinUtc { get; set; }
+        public ProcesoEstado Estado { get; set; } = ProcesoEstado.Pendiente;
+
+        public DateTime CreadoUtc { get; set; } = DateTime.UtcNow;
+
+        public List<Eleccion> Elecciones { get; set; } = new();
+    }
+}

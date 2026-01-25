539de978519a721ec8e0c163c2f6614defc84b2f
Cristhiansito0 <cristhianguajan2@gmail.com>
2026-01-25 18:44:57 -0500

    Cambios en el controlador BaseApi

--- VotoElectonico/Controllers/BaseApiController.cs
+++ VotoElectonico/Controllers/BaseApiController.cs
@@ -1,18 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace VotoElectonico.Controllers
 {
+    [ApiController]
     public abstract class BaseApiController : ControllerBase
     {
         protected string? GetCedula() => User.FindFirst("cedula")?.Value;
 
         protected Guid? GetUserId()
         {
-            var sub = User.FindFirst(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub)?.Value;
-            return Guid.TryParse(sub, out var id) ? id : null;
+            var id =
+                User.FindFirstValue(ClaimTypes.NameIdentifier) ??
+                User.FindFirstValue(JwtRegisteredClaimNames.Sub);
+
+            return Guid.TryParse(id, out var g) ? g : null;
         }
 
         protected bool IsInRole(string role) => User.IsInRole(role);
     }
+
 }

4fb58634e5b820b202c9db25df7c61d3be9ef9f6
Enma <c.enma69@gmail.com>
2026-02-02 10:23:34 -0500

    Se creo la clase CrearEleccion

---
+++ VotoElect.MVC/ApiContracts/EleccionesDtos.cs
@@ -0,0 +1,26 @@
+﻿namespace VotoElect.MVC.ApiContracts;
+
+public class CrearEleccionRequestDto
+{
+    public string ProcesoElectoralId { get; set; } = "";
+    public string Tipo { get; set; } = "";
+    public string Titulo { get; set; } = "";
+    public int? MaxSeleccionIndividual { get; set; }
+}
+
+public class CrearListaRequestDto
+{
+    public string EleccionId { get; set; } = "";
+    public string Nombre { get; set; } = "";
+    public string Codigo { get; set; } = "";
+    public string? LogoUrl { get; set; }
+}
+
+public class CrearCandidatoRequestDto
+{
+    public string EleccionId { get; set; } = "";
+    public string NombreCompleto { get; set; } = "";
+    public string? Cargo { get; set; }
+    public string FotoUrl { get; set; } = "";
+    public string? PartidoListaId { get; set; }
+}

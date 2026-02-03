35943b0867037f1a5e522fe5bbe4a8fd824c7c1b
karliQ1345 <karlaquelal83@gmail.com>
2026-02-03 12:18:14 -0500

    Nueva migracion por que se agrego una nueva propiedad a el modelo de comprobrante

---
+++ VotoElectonico/Migrations/20260203170236_AddPublicTokenToComprobante.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VotoElectonico.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublicTokenToComprobante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicToken",
+                table: "ComprobantesVoto",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublicTokenExpiraUtc",
+                table: "ComprobantesVoto",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicToken",
+                table: "ComprobantesVoto");
+
+            migrationBuilder.DropColumn(
+                name: "PublicTokenExpiraUtc",
+                table: "ComprobantesVoto");
+        }
+    }
+}

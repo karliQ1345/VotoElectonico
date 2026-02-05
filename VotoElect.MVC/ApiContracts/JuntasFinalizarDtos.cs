19483eed17dfb9dfcc7ae1deefc999c72d6b4d9a
Cristhiansito0 <cristhianguajan2@gmail.com>
2026-02-05 07:26:43 -0500

    Se crea el DTO para finalizar las juntas en el mvc

---
+++ VotoElect.MVC/ApiContracts/JuntasFinalizarDtos.cs
@@ -0,0 +1,15 @@
+﻿namespace VotoElect.MVC.ApiContracts
+{
+    public class FinalizarJuntaRequestDto
+    {
+        public string ProcesoElectoralId { get; set; } = "";
+    }
+
+    public class FinalizarJuntaResponseDto
+    {
+        public bool Ok { get; set; }
+        public string Mensaje { get; set; } = "";
+        public string JuntaCodigo { get; set; } = "";
+        public DateTime? CerradaUtc { get; set; }
+    }
+}
